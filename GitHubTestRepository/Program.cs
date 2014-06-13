53a2779c43434eb4c40ec1b46b872c08e817b568
rgmakinson <rgmakinson@gmail.com>
2014-06-13 12:00:03 +0100

    First push
    Push all the solution, project and source files. Update .gitignore to
    ignore the rest

---
+++ GitHubTestRepository/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
